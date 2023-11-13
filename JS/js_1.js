e4a043d82d35d8a8ef863e80f2ffe7b2b8fdcf8d
luvaluva1 <vrodrigue735@gmail.com>
2023-11-13 19:43:32 -0300

    Avance en la lógica de la bola

--- JS/js_1.js
+++ JS/js_1.js
@@ -1,27 +1,27 @@
-const width= 28;
-const grid= document.querySelector(".grid");
+const width = 28;
+const grid = document.querySelector(".grid");
 const scoreDisplay = document.getElementById("score");
-let score= 0;
-const squares=[];
+let score = 0;
+const squares = [];
 
 var BallDirs = {
-	NONE : 0,
-	LEFT : 1,
-	RIGHT : 2,
-	UP : 4,
-	DOWN : 8
+  NONE: 0,
+  LEFT: 1,
+  RIGHT: 2,
+  UP: 4,
+  DOWN: 8
 };
-function Ball(x, y, radius, dir, speed) {
-	this.x = x;
-	this.y = y;
-	this.radius = radius;
-	this.dir = BallDirs.NONE;
-	this.speed = speed;
-}
-var BALL_RADIUS = 6;
-	var BALL_DEFAULT_SPEED = 4;
-	var BALL_MAX_SPEED = 5;
 
+function Ball(x, y, radius, dir, speed) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.dir = dir;
+  this.speed = speed;
+}
+
+var BALL_RADIUS = 6;
+var BALL_DEFAULT_SPEED = 4;
 
 const layout=[
     1,1,1,1,1,1,1,1,
@@ -74,7 +74,92 @@ function createPacBoard(){
     createPacBoard();
     let pacmanCurrentIndex=163;
     squares[pacmanCurrentIndex].classList.add("pacman");
+
+    const ball = new Ball(150, 150, BALL_RADIUS, BallDirs.RIGHT | BallDirs.DOWN, BALL_DEFAULT_SPEED);
+
+    function drawBall() {
+        const ballElement = document.createElement("div");
+        ballElement.classList.add("ball");
+        ballElement.style.width = ball.radius * 2 + "px";
+        ballElement.style.height = ball.radius * 2 + "px";
+        ballElement.style.borderRadius = "50%";
+        ballElement.style.backgroundColor = "white";
+        ballElement.style.position = "absolute";
+        ballElement.style.left = ball.x + "px";
+        ballElement.style.top = ball.y + "px";
+        grid.appendChild(ballElement);
+      }
+
+      function moveBall() {
+        if (ball.dir & BallDirs.LEFT) {
+          ball.x -= ball.speed;
+        } else if (ball.dir & BallDirs.RIGHT) {
+          ball.x += ball.speed;
+        }
+      
+        if (ball.dir & BallDirs.UP) {
+          ball.y -= ball.speed;
+        } else if (ball.dir & BallDirs.DOWN) {
+          ball.y += ball.speed;
+        }
+
+    // Verificar colisiones con las paredes
+    if (ball.x - ball.radius <= 0 && ball.dir & BallDirs.LEFT) {
+        ball.dir ^= BallDirs.LEFT;
+    } else if (ball.x + ball.radius >= grid.offsetWidth && ball.dir & BallDirs.RIGHT) {
+        ball.dir ^= BallDirs.RIGHT;
+    }
+
+    if (ball.y - ball.radius <= 0 && ball.dir & BallDirs.UP) {
+        ball.dir ^= BallDirs.UP; 
+    } else if (ball.y + ball.radius >= grid.offsetHeight && ball.dir & BallDirs.DOWN) {
+        ball.dir ^= BallDirs.DOWN; 
+    }
+
+    // Verificar colisiones con el jugador (pacman)
+    if (
+        ball.x + ball.radius >= squares[pacmanCurrentIndex].offsetLeft &&
+        ball.x - ball.radius <= squares[pacmanCurrentIndex].offsetLeft + squares[pacmanCurrentIndex].offsetWidth &&
+        ball.y + ball.radius >= squares[pacmanCurrentIndex].offsetTop &&
+        ball.y - ball.radius <= squares[pacmanCurrentIndex].offsetTop + squares[pacmanCurrentIndex].offsetHeight
+    ) {
+        ball.dir ^= BallDirs.UP; // Invertir el bit UP
+    }
+
+
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i].classList.contains("wall")) {
+            if (
+                ball.x + ball.radius >= squares[i].offsetLeft &&
+                ball.x - ball.radius <= squares[i].offsetLeft + squares[i].offsetWidth &&
+                ball.y + ball.radius >= squares[i].offsetTop &&
+                ball.y - ball.radius <= squares[i].offsetTop + squares[i].offsetHeight
+            ) {
+                
+                squares[i].classList.remove("wall");
+                score++;
+                scoreDisplay.textContent = score;
+               
+                if (ball.dir & BallDirs.UP) ball.dir ^= BallDirs.UP;
+                if (ball.dir & BallDirs.DOWN) ball.dir ^= BallDirs.DOWN;
+                if (ball.dir & BallDirs.LEFT) ball.dir ^= BallDirs.LEFT;
+                if (ball.dir & BallDirs.RIGHT) ball.dir ^= BallDirs.RIGHT;
+            }
+        }
+    }
+
+    // Mover la bola en la pantalla
+    const ballElement = document.querySelector(".ball");
+    ballElement.style.left = ball.x + "px";
+    ballElement.style.top = ball.y + "px";
+
     
+    requestAnimationFrame(moveBall);
+}
+
+drawBall();
+moveBall();
+
 
 let leftKeyPressed = false;
 let rightKeyPressed = false;
@@ -127,10 +212,12 @@ function movePacman() {
     setTimeout(movePacman, velocidad);
 }
 
-movePacman();
-
-gameLoop();
+function gameLoop() {
+    movePacman();
+    moveBall();
     
+    requestAnimationFrame(gameLoop);
+        
     document.addEventListener("keyup", control);
     
     function pacDotEaten() {
@@ -139,4 +226,8 @@ gameLoop();
             scoreDisplay.textContent = score;
             squares[pacmanCurrentIndex].classList.remove('amarillo');
         }
+        movePacman();
     }
+  }
+
+gameLoop();
